bd59238b323037644f1306c5e521940b44caa6db
Sukhpreet Sekhon <ersukhpreetsekhon03@gmail.com>
2015-10-26 22:53:47 -0400

    added gameobject and script for multiple moving coins

--- Assets/_Scripts/CoinController.cs
+++ Assets/_Scripts/CoinController.cs
@@ -1,17 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CoinController : MonoBehaviour {
+[System.Serializable]
+public class Speed
+{
+    public float minSpeed, maxSpeed;
+}
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+[System.Serializable]
+public class Drift
+{
+    public float minDrift, maxDrift;
+}
+
+[System.Serializable]
+public class Boundary
+{
+    public float minX, maxX, minY, maxY;
+}
+
+public class CoinController : MonoBehaviour {
+    //public instance variable
+    public Speed speed;
+    public Drift drift;
+    public Boundary boundary;
+
+    //private instance variable
+    private float _Currentspeed;
+    private float _Currentdrift;
+
+    private AudioSource _EndEnemy;
+    // Use this for initialization
+    void Start()
+    {
+
+        this._Reset();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 currentPosition = new Vector2(0.0f, 0.0f);
+        currentPosition = gameObject.GetComponent<Transform>().position;
+        currentPosition.y += this._Currentdrift;
+        currentPosition.x -= this._Currentspeed;
+        //movve the gameobject to th ecurrent position
+        gameObject.GetComponent<Transform>().position = currentPosition;
+
+        //bottom boundray check oceans meets camra view point
+        if (currentPosition.x <= boundary.minX)
+        {
+            this._Reset();
+        }
+
+    }
+    //resets our game object
+    private void _Reset()
+    {
+        this._Currentdrift = Random.Range(drift.minDrift, drift.maxDrift);
+        this._Currentspeed = Random.Range(speed.minSpeed, speed.maxSpeed);
+        Vector2 resetPosition = new Vector2(boundary.maxX, Random.Range(boundary.minY, boundary.maxY));
+        gameObject.GetComponent<Transform>().position = resetPosition;
+    }
 
     void OnCollisionEnter2D(Collision2D otherCollider)
     {
---
+++ Assets/_Scripts/CoinObjectScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinObjectScript : MonoBehaviour {
+    //public instance variable
+    public int coinCount;
+    public GameObject coin;
+
+
+	// Use this for initialization
+	void Start () {
+        this._GenerateCoins();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //generate cloud
+    private void _GenerateCoins()
+    {
+        for (int count = 0; count < this.coinCount; count++)
+        {
+            Instantiate(coin);
+        }
+    }
+}
